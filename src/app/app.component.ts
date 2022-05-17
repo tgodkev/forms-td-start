a513615cfee756f601f584acc2e829677e649095
kevin knight <tgodkev@gmail.com>
2022-05-17 17:52:14 -0400

    two way binding added for validation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // view child to get access to local ref.
+  @ViewChild('f') signupForm: NgForm;
+
+  // for drop down options.
+  defaultQuestion = 'pet';
+
+  // two way binding for drop down question
+  answer = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+// on submit method for submit form.
+  // need ngForm.
+  // onSubmit(form: NgForm) {
+  // console.log(form);
+  // }
+
+  // alternative using view child to get local ref.
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
